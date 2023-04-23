e80422e9275add60f53b15156b7284f896dd51aa
Aaron Frazier Jr <afraz91@wgu.edu>
2023-04-23 01:42:11 +0000

    I added the main.cs file

--- main.cs
+++ main.cs
@@ -1,7 +1,7 @@
 //Console.WriteLine("Hello World!");
 Console.Write("Congratulations!");
 Console.Write(" ");
-Console.Write("You wrote yourt first lines of code.");
+Console.Write("You wrote your first lines of code.");
 
 Console.WriteLine("This is the first line.");
 
