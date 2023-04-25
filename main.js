49516b841a338a225a3246bc8686825d6a24eafc
Aaron Frazier Jr <afraz91@wgu.edu>
2023-04-24 18:30:23 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 // Date: 2020/04/04
 console.log("Hello World");
+
+//console.log(10 % 3); // => 1
+console.log(14 % 5); // => 4
+console.log(20 % 17); // => 3
+console.log(18 % 6); // => 0
+console.log(7 % 9); // => 7 
+//
