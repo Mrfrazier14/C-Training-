4eb9021585bec390c4635e6f7d5ef6bd9222d0dd
Aaron Frazier Jr <afraz91@wgu.edu>
2023-04-24 17:53:43 -0700

    I will update these

--- Program.cs
+++ Program.cs
@@ -2,3 +2,8 @@
 using system;
 
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello World!");
+Console.WriteLine("I am Learning C#");
+Console.WriteLine("It is awesome!");
+
+// I will be adding more code here soon
