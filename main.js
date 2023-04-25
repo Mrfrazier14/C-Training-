606dca1dcf41721259c95ca48488faedf210cfcc
Aaron Frazier Jr <afraz91@wgu.edu>
2023-04-25 09:39:36 -0700

    Update main.js
    I removed my old JS because this was added by mistake

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
 // Date: 2020/04/04
 console.log("Hello World");
 
-//console.log(10 % 3); // => 1
-//console.log(14 % 5); // => 4
-//console.log(20 % 17); // => 3
-//console.log(18 % 6); // => 0
-//console.log(7 % 9); // => 7 
-// modulo has precedence over addition
-//console.log(4 + 12 % 5); // => 6
-//console.log((4 + 12) % 5); // => 1
 
